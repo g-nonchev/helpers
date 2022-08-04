29bd736bc224426224c0cf74befc1f07e2b1fb11
g-nonchev <genonchev@gmail.com>
2022-08-04 09:28:39 +0300

    Wp helpers

---
+++ wp/warp_ajax_product_remove.php
@@ -0,0 +1,39 @@
+
+<?php
+// Remove product in the cart using ajax
+function warp_ajax_product_remove()
+{
+	// Get mini cart
+	ob_start();
+
+	foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
+		if ($cart_item['product_id'] == $_POST['product_id'] && $cart_item_key == $_POST['cart_item_key']) {
+			WC()->cart->remove_cart_item($cart_item_key);
+		}
+	}
+
+	WC()->cart->calculate_totals();
+	WC()->cart->maybe_set_cart_cookies();
+
+	woocommerce_mini_cart();
+
+	$mini_cart = ob_get_clean();
+
+	// Fragments and mini cart are returned
+	$data = array(
+		'fragments' => apply_filters(
+			'woocommerce_add_to_cart_fragments',
+			array(
+				'div.widget_shopping_cart_content' => '<div class="widget_shopping_cart_content">' . $mini_cart . '</div>'
+			)
+		),
+		'cart_hash' => apply_filters('woocommerce_add_to_cart_hash', WC()->cart->get_cart_for_session() ? md5(json_encode(WC()->cart->get_cart_for_session())) : '', WC()->cart->get_cart_for_session())
+	);
+
+	wp_send_json($data);
+
+	die();
+}
+
+add_action('wp_ajax_product_remove', 'warp_ajax_product_remove');
+add_action('wp_ajax_nopriv_product_remove', 'warp_ajax_product_remove');
